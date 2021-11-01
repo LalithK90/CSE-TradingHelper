38471fb1fb717ebc1d4f84c28d4422a4b5be4aec
LalithK90 <asakahatapitiya@gmail.com>
2021-11-01 19:28:34 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$("#wPrice").bind('keyup', function () {
+    minimumSellPrice();
+});
+
+$("#bFee").bind('keyup', function () {
+    minimumSellPrice();
+});
+
+function minimumSellPrice () {
+    var wPrice = $("#wPrice").val();
+    var bFee = $("#bFee").val();
+    if (wPrice) {
+        swal("Warning", "Please enter the buying price", "warning");
+    } else {
+        var minSellPrice = (wPrice * bFee) + wPrice;
+        minSellPrice = parseFloat(minSellPrice).toFixed(2);
+        $("#minSellPrice").val(minSellPrice);
+        if (minSellPrice < wPrice) {
+            $("#minSellPrice").css({ "color": "red", "background-color": "black" });
+        } else {
+            $("#minSellPrice").css({ "color": "green", "background-color": "white" });
+        }
+    }
+}
